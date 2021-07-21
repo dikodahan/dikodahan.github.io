5e6d407e1512e57e7acaa879c659992f11c4bd15
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2021-07-21 18:43:17 -0400

    Add files via upload

---
+++ creative/files/hebkb.sh
@@ -0,0 +1,4 @@
+ime enable org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService
+
+ime set org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService
+
