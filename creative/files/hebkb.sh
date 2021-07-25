d654870976c6114f4f2c500c897036dc71934407
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2021-07-25 16:59:34 -0400

    Update hebkb.sh

--- creative/files/hebkb.sh
+++ creative/files/hebkb.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 
-exec "ime enable org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService"
-
-exec "ime set org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService"
+ime enable org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService
+ime set org.liskovsoft.androidtv.rukeyboard/com.liskovsoft.leankeyboard.ime.LeanbackImeService
 
